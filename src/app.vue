95c3f7399f56769f9a3f7a1dd2ec72249a791fba
hhhhhq <Mr.j@tom.com>
2019-11-19 10:07:38 +0800

    init prepare

---
+++ src/app.vue
@@ -0,0 +1,19 @@
+<template>
+    <div id="app">{{text}}</div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            text: 'abcd'
+        }
+    }
+}
+</script>
+
+<style>
+    #app {
+        color: #c93942;
+    }
+</style>
